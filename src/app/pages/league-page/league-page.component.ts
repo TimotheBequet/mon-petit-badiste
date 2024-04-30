a8e2e0f5219fb62ce0125f693e0df382ea8e5f32
timothe.bequet <timothe.bequet@cgi.com>
2024-04-30 18:01:26 +0200

    début codage page league

--- src/app/pages/league-page/league-page.component.ts
+++ src/app/pages/league-page/league-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import {LeaguesInterface} from "../../interfaces/leagues.interface";
+import {map} from "rxjs";
 
 @Component({
   selector: 'app-league-page',
   templateUrl: './league-page.component.html',
-  styleUrls: ['./league-page.component.scss']
+  styleUrls: ['./league-page.component.scss'],
+  host: {
+    class: 'div-all-screen'
+  }
 })
-export class LeaguePageComponent {
+export class LeaguePageComponent implements OnInit {
+  league: LeaguesInterface | null = null;
+  link: string = "/home";
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap
+      .pipe(map(() => window.history.state))
+      .subscribe(league => this.league = league);
+  }
 }
