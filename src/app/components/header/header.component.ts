58c6a39f6a662c4f04c421ea741864c43787d3d0
timothe.bequet <timothe.bequet@cgi.com>
2025-05-22 17:11:10 +0200

    fix menu + resizing header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { UserInterface } from 'src/app/interfaces/user.interface';
 import { UserService } from 'src/app/services/user.service';
 
@@ -19,10 +21,22 @@ export class HeaderComponent implements OnInit {
   maxFontSize: number = 48;
   minFontSize: number = 21;
   fontSize: number = this.maxFontSize;
+  innerWidth: number = 0;
+  animateHeader: boolean = false;
 
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService, public router: Router) {
+    router.events.pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd)).subscribe((e) => {
+      this.animateHeader = (e.url == '/home' && !this.userService.isUserLogged());  
+    });
+  }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth <= 500) {
+      this.maxFontSize = 35;
+      this.minFontSize = 15;
+      this.fontSize = this.maxFontSize;
+    }
     this.userService.user$.subscribe(user => {
       this.user = this.userService.getUser();
       this.isUserLogged = this.userService.isUserLogged();
@@ -30,19 +44,38 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+  @HostListener('window:resize', [])
+  onResize(): void {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth <= 500) {
+      this.maxFontSize = 35;
+      this.minFontSize = 15;
+      this.fontSize = this.maxFontSize;
+    } else {
+      this.maxFontSize = 48;
+      this.minFontSize = 21;
+      this.fontSize = this.maxFontSize;
+    }
+  }
+
   @HostListener('window:scroll', [])
   onWindowScroll(): void {
-    const scrollY: number = window.scrollY || document.documentElement.scrollTop;
-    if (scrollY <= this.scrollStart) {
-      this.navHeight = this.maxNavHeight;
-      this.fontSize = this.maxFontSize;
-    } else if (scrollY >= this.scrollEnd) {
-      this.navHeight = this.minNavHeight;
-      this.fontSize = this.minFontSize;
-    } else {
-      const progress: number = (scrollY - this.scrollStart) / (this.scrollEnd - this.scrollStart);
-      this.navHeight = this.maxNavHeight - progress * (this.maxNavHeight - this.minNavHeight);
-      this.fontSize = this.navHeight * 0.428;
+    if (this.animateHeader) {
+      const scrollY: number = window.scrollY || document.documentElement.scrollTop;
+      if (scrollY <= this.scrollStart) {
+        this.navHeight = this.maxNavHeight;
+        this.fontSize = this.maxFontSize;
+      } else if (scrollY >= this.scrollEnd) {
+        this.navHeight = this.minNavHeight;
+        this.fontSize = this.minFontSize;
+      } else {
+        const progress: number = (scrollY - this.scrollStart) / (this.scrollEnd - this.scrollStart);
+        this.navHeight = this.maxNavHeight - progress * (this.maxNavHeight - this.minNavHeight);
+        this.fontSize = this.navHeight * 0.428;
+        if (this.innerWidth <= 500) {
+          this.fontSize = this.fontSize * 0.73;
+        }
+      }
     }
   }
 }
